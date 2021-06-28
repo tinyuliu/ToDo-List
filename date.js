ab19e941016fd0e73b6921c0ad8c388d10cc04eb
tinyuliu <tinyu1016@gmail.com>
2021-06-28 08:02:36 +0800

    complete veresion 1

---
+++ date.js
@@ -0,0 +1,30 @@
+//jshint esversion:6
+
+//Refactory
+
+//anoymous function
+exports.getDate = function() {
+    const today = new Date();
+    const currentDay = today.getDay();
+
+    const options = {
+        weekday:"long",
+        day:"numeric",
+        month: "long"
+    };
+
+    return today.toLocaleDateString("en-US", options);
+
+};
+
+exports.getDay = function() {
+    const today = new Date();
+    const currentDay = today.getDay();
+
+    const options = {
+        weekday:"long"
+    };
+
+    return today.toLocaleDateString("en-US", options);
+
+}
